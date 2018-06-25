5a15f9fdcc7da1b0b4358b639b424ab34200c9c5
JKJUN527 <631642753@qq.com>
2018-06-25 21:22:38 +0800

    添加站点自动推送功能

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -59,6 +59,20 @@
     //news滚动效果
     jQuery(".notice").slide({mainCell:".bd ul",autoPage:true,effect:"top",autoPlay:true,vis:3});
 </script>
+<script>
+    (function(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        }
+        else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
+</script>
 @section('custom-script')
 @show
 </body>
